1b40f86a6b9adf99acac45f35ae9e6b3bcb9f6bc
Michel Domingos <micheldslive@gmail.com>
2024-02-24 21:19:07 -0300

    feat: adding initial tailwind settings to workspace

---
+++ configs/tailwind-config/tailwind.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["../../packages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+};
+
+export default config;
