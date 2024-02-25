f6e82ab22c992172f814e17dc81434c78af34654
Michel Domingos <micheldslive@gmail.com>
2024-02-25 20:02:54 -0300

    feat: creating frame component / story

---
+++ packages/components/src/components/Frame/Frame.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, forwardRef } from "react"
+import { tv } from "tailwind-variants"
+
+export interface FrameProps extends Omit<ComponentProps<"div">, "ref"> {}
+
+const frameVariants = tv({
+  base: "p-5 border border-solid border-neutral-200 rounded-md overflow-hidden",
+})
+
+export const Frame = forwardRef<HTMLDivElement, FrameProps>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <section className={frameVariants({ className })} ref={ref} {...props}>
+        {children}
+      </section>
+    )
+  },
+)
