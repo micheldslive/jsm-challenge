fbbc0d3b56915ad127501981e49ef029a84b6a54
Michel Domingos <micheldslive@gmail.com>
2024-02-24 21:33:35 -0300

    chore: workspace settings changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@jsm/eslint-config/library.js"],
+  ignorePatterns: ["apps/**", "packages/**", "configs/**"],
+  extends: ["@jsm/eslint-configlibrary.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
