960195734a58687148386eb957dc30c55ffe00e0
Michel Domingos <micheldslive@gmail.com>
2024-02-24 21:23:13 -0300

    feat: creating component project package

---
+++ packages/components/.eslintrc.cjs
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@jsm/eslint-config/vite.js"],
+  parser: "@typescript-eslint/parser",
+};
---
+++ packages/components/postcss.config.js
@@ -0,0 +1,1 @@
+export { default } from "@jsm/tailwind-config/postcss.config";
