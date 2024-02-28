8e66265554d0c5fe8ece83277009774e1010d944
Michel Domingos <micheldslive@gmail.com>
2024-02-27 22:48:36 -0300

    chore: adding global css and new settings in tailwind

--- configs/tailwind-config/tailwind.config.ts
+++ configs/tailwind-config/tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
         "neutral-350": "#B0B0B0",
       },
     },
+    transitionTimingFunction: {
+      "in-select": "cubic-bezier(.61,-0.53,.43,1.43)",
+    },
+    transformOrigin: {
+      half: "50% 50%",
+    },
+    keyframes: {
+      stroke: {
+        "100%": { strokeDashoffset: "0" },
+      },
+    },
+    animation: {
+      stroke: "stroke 1s cubic-bezier(0.65, 0, 0.45, 1) 0.1s forwards",
+    },
   },
 }
 
