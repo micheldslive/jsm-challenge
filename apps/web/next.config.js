febf8c4ce5fc571505abccd6dae3a473d943747a
Michel Domingos <micheldslive@gmail.com>
2024-03-02 16:49:28 -0300

    chore: changing tailwind settings for web app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@jsm/components"],
+  transpilePackages: ["@jsm/ui"],
 };
--- apps/web/tailwind.config.cjs
+++
@@ -1,1 +0,0 @@
-module.exports = require("@jsm/tailwind-config/tailwind.config")
