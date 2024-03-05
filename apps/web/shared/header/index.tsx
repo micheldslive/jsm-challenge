43f88a17687b1dd15e14007ebc5ec964ff41530d
Michel Domingos <micheldslive@gmail.com>
2024-03-05 12:55:25 -0300

    chore: scope changes in shared

--- apps/web/shared/header/index.tsx
+++ apps/web/shared/header/index.tsx
@@ -8,7 +8,7 @@ import { Theme } from "./Theme"
 
 export const Header = () => {
   return (
-    <header className='bg-neutral-100 dark:bg-neutral-750 sticky top-0'>
+    <header className='bg-neutral-100 dark:bg-neutral-750 sticky top-0 z-50'>
       <Container className='lg:container py-6 flex flex-col gap-4 items-center justify-between md:!flex-row md:gap-0'>
         <JSMLogo />
         <SearchForm />
