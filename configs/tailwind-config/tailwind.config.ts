4e046e94c41f456755f89e8ad676cc4d444bae11
Michel Domingos <micheldslive@gmail.com>
2024-03-05 18:26:08 -0300

    chore: settings changes

--- configs/tailwind-config/tailwind.config.ts
+++ configs/tailwind-config/tailwind.config.ts
@@ -14,12 +14,21 @@ const config: Config = {
         "neutral-360": "#4f4f4f",
         "neutral-750": "#3A3A3A",
         "neutral-760": "#c5c5c5",
+        "blue-social": "#28B0FC",
       },
+    },
+    screens: {
+      rsm: { max: "639px" },
+      rmd: { max: "767px" },
+      rlg: { max: "1023px" },
     },
     container: {
       screens: {
         jsm: "1104px",
       },
+    },
+    transitionProperty: {
+      "max-h": "max-height",
     },
     transitionTimingFunction: {
       "in-select": "cubic-bezier(.61,-0.53,.43,1.43)",
@@ -31,9 +40,15 @@ const config: Config = {
       stroke: {
         "100%": { strokeDashoffset: "0" },
       },
+      skeleton: {
+        "50%": {
+          opacity: "0.5",
+        },
+      },
     },
     animation: {
       stroke: "stroke 1s cubic-bezier(0.65, 0, 0.45, 1) 0.1s forwards",
+      skeleton: "skeleton 2s cubic-bezier(.4,0,.6,1) infinite",
     },
   },
   plugins: [ta],
